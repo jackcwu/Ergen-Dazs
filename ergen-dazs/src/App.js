a7cb7dcba6fd9e8f129990f33e920598ec2d8cda
shanjng <shanjng@umich.edu>
2021-01-17 01:22:11 -0500

    fixed calibration error

--- ergen-dazs/src/App.js
+++ ergen-dazs/src/App.js
@@ -18,79 +18,86 @@ const App = () => {
   const [loadingModel, setLoadingModel] = useState(true);
   const [showCalibrationPane, setShowCalibrationPane] = useState(false);
   const [showDetectionPane, setShowDetectionPane] = useState(false);
-  const [calibrationData, setCalibrationData] = useState(false);
+  const [calibrated, setCalibrated] = useState(false);
+
+  const setupModel = async () => {
+    setWasmPath(
+      'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@latest/dist/tfjs-backend-wasm.wasm'
+    );
+    await tf.setBackend('wasm');
+    const model = await blazeface.load();
+    setLoadingModel(false);
+    return model;
+  };
+
+  const runFacedetect = (model) => {
+    const timerId = setInterval(() => {
+      detect(model);
+    }, 100);
+    return timerId;
+  };
+
+  const detect = async (model) => {
+    // check if webcam is ready and has enough data (HAVE_ENOUGH_DATA === 4)
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make detections
+      const returnTensors = false;
+      const flipHorizontal = true;
+      const predictions = await model.estimateFaces(
+        video,
+        returnTensors,
+        flipHorizontal
+      );
+
+      // Get canvas context
+      const ctx = canvasRef.current.getContext('2d');
+      requestAnimationFrame(() => drawBoundingBox(predictions, ctx));
+    }
+  };
 
   useEffect(() => {
-    console.log('called useeffect');
-
-    const runFacedetect = async () => {
-      setWasmPath(
-        'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@latest/dist/tfjs-backend-wasm.wasm'
-      );
-      await tf.setBackend('wasm');
-      const model = await blazeface.load();
-      setLoadingModel(false);
-      console.log('model loaded');
-
-      const timerId = setInterval(() => {
-        detect(model);
-      }, 100);
-      return timerId;
-      // detect(model);
+    console.log('mounted');
+    var timerId;
+    const setup = async () => {
+      const myModel = await setupModel();
+      timerId = runFacedetect(myModel);
     };
 
-    const detect = async (model) => {
-      // check if webcam is ready and has enough data (HAVE_ENOUGH_DATA === 4)
-      if (
-        typeof webcamRef.current !== 'undefined' &&
-        webcamRef.current !== null &&
-        webcamRef.current.video.readyState === 4
-      ) {
-        // Get Video Properties
-        const video = webcamRef.current.video;
-        const videoWidth = webcamRef.current.video.videoWidth;
-        const videoHeight = webcamRef.current.video.videoHeight;
-
-        // Set video width
-        webcamRef.current.video.width = videoWidth;
-        webcamRef.current.video.height = videoHeight;
-
-        // Set canvas width
-        canvasRef.current.width = videoWidth;
-        canvasRef.current.height = videoHeight;
-
-        // Make detections
-        const returnTensors = false;
-        const flipHorizontal = true;
-        const predictions = await model.estimateFaces(
-          video,
-          returnTensors,
-          flipHorizontal
-        );
-        // console.log(predictions);
-        if (calibrationData) {
-          console.log('inside: ', predictions);
-          setCalibrationData(false);
-        }
-
-        // Get canvas context
-        const ctx = canvasRef.current.getContext('2d');
-        requestAnimationFrame(() => drawBoundingBox(predictions, ctx));
-      }
-    };
-
-    const timerId = runFacedetect();
+    setup();
 
     return () => clearInterval(timerId);
-  }, [calibrationData]);
+  }, []);
 
-  useEffect(() => {
-    console.log('every render');
-    console.log('calibrationdata on render', calibrationData);
-  });
+  const handleCalibrate = async () => {
+    const model = await setupModel();
 
-  const calibrate = () => {
-    setCalibrationData(true);
+    if (model) {
+      const predictions = await model.estimateFaces(
+        webcamRef.current.video,
+        false,
+        true
+      );
+      console.log('predictions: ', predictions);
+    }
+
+    setCalibrated(true);
   };
 
   return (
@@ -104,7 +111,7 @@ const App = () => {
             <canvas ref={canvasRef} className='canvas'></canvas>
           </div>
 
-          <button onClick={calibrate}>c'mon</button>
+          <button onClick={handleCalibrate}>c'mon</button>
 
           {showCalibrationPane && <CalibrationPane></CalibrationPane>}
 
